bb870fc046e4074fe083d8ae88a4fab40eaedea0
Joan Britto <joan@cocoalabs.in>
2020-04-08 12:00:47 +0530

    changes on layout

--- backend/theme/layouts/left.php
+++ backend/theme/layouts/left.php
@@ -1,4 +1,8 @@
 <?php
+
+use yii\helpers\Url;
+use yii\web\View;
+
 $role = yii::$app->user->identity->role;
 ?>
 <aside class="main-sidebar">
@@ -20,7 +24,7 @@ $role = yii::$app->user->identity->role;
                     ['label' => 'Projects', 'icon' => 'file-code-o', 'url' => ['/projects'],'visible' => ($role=='admin') ? true : false],
                     ['label' => 'Administrators', 'icon' => 'user-circle-o', 'url' => ['/admins'],'visible' => ($role=='super-admin') ? true : false],
                     ['label' => 'Login', 'url' => ['site/login'], 'visible' => Yii::$app->user->isGuest],                   
-                    ['label' => 'Logout','icon'=>'sign-out','data-method' => 'post', 'url' => ['site/logout'], 'visible' => !Yii::$app->user->isGuest],                   
+                    ['label' => 'Logout','icon'=>'sign-out','url' => ['site/logout'], 'visible' => !Yii::$app->user->isGuest,'options'=>['class'=>'logout']],                   
                 ],
             ]
         ) ?>
